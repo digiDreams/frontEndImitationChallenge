1566e547b32e1150834bfd7f742c4c51a949de80
digiDreams <digital2dreamer@gmail.com>
2022-07-30 17:22:49 +0200

    Push project before getting started

---
+++ frontEndImitationChallenge/frontEndImitationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontEndImitationChallengeApp.swift
+//  frontEndImitationChallenge
+//
+//  Created by apprenant55 on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct frontEndImitationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
